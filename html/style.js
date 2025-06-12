8ef2fd52d7dad7da66b25353c2150f6e7265055c
0tohero99 <shubhamsinghhindustan@gmail.com>
2025-06-12 10:18:59 -0700

    .

--- html/style.js
+++ html/style.js
@@ -1,10 +1,10 @@
 const showcoupons = () => {
     setTimeout(function () {
         document.getElementById("coupons").style.display = "block"
-        document.getElementsByTagName("section")[0].style.opacity = ".3"
+        // document.getElementsByTagName("section")[0].style.opacity = ".3"
     }, 5000)
 }
 const closecoupons = () => {
     document.getElementById("coupons").style.display = "none"
-//     document.getElementsByTagName("section")[0].style.opacity = "1"
+    // document.getElementsByTagName("section")[0].style.opacity = "1"
 }
